e8a82c4f004b928743bae5466a4d95adb8f4db18
leapingjagg-dev <jiwon.song@snu.ac.kr>
2025-06-12 10:28:42 +0000

    test

--- lunchpoll-bot.py
+++ lunchpoll-bot.py
@@ -79,7 +79,8 @@ def slack_events():
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(send_poll, "cron", hour=2, minute=20, day_of_week='mon-fri') # UTC+9
-scheduler.add_job(send_poll, "cron", hour=10, minute=18, day_of_week='mon-fri') # UTC+9
+scheduler.add_job(send_poll, "cron", hour=10, minute=33, day_of_week='mon-fri') # UTC+9
+scheduler.add_job(send_poll, "cron", hour=19, minute=33, day_of_week='mon-fri') # UTC+9
 scheduler.start()
 
 if __name__ == "__main__":
